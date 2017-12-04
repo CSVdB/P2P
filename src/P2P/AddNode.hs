28702e35e843f3bba54d6577560d465b169208b7
Nick Van den Broeck <nick.van.den.broeck666@gmail.com>
2017-12-04 10:14:41 -0500

    "get" and "put" can now be parsed from terminal This still needs to be tested

--- src/P2P/AddNode.hs
+++ src/P2P/AddNode.hs
@@ -7,7 +7,9 @@ import Import
 import Control.Concurrent (forkIO)
 
 import P2P.Chan
+import P2P.Get
 import P2P.JSONUtils
+import P2P.KeyValPair
 import P2P.Put
 import P2P.SockAddr
 import P2P.SocketInfo
@@ -44,24 +46,37 @@ runConn conn addrChan store = do
 
 decodeMsg :: ByteString -> AddrChan -> StoreChan -> IO ()
 decodeMsg msg addrChan store = do
-    decodeMsgAsAddr msg addrChan
-    decodeMsgAsAddrs msg addrChan
-    decodeMsgAsKeyVal msg addrChan store
+    isAddr <- decodeMsgAsAddr msg addrChan
+    isAddrs <- decodeMsgAsAddrs msg addrChan
+    isKeyVal <- decodeMsgAsKeyVal msg addrChan store
+    when (isAddr || isAddrs || isKeyVal) $
+        decodeMsgAsTerminalInput msg addrChan store
 
-decodeMsgAsAddr :: ByteString -> AddrChan -> IO ()
+decodeMsgAsAddr :: ByteString -> AddrChan -> IO Bool
 decodeMsgAsAddr msg addrChan =
     case decode msg of
-        Just addr -> write addrChan addr
-        Nothing -> pure ()
+        Just addr -> const True <$> write addrChan addr
+        Nothing -> pure False
 
-decodeMsgAsAddrs :: ByteString -> AddrChan -> IO ()
+decodeMsgAsAddrs :: ByteString -> AddrChan -> IO Bool
 decodeMsgAsAddrs msg addrChan =
     case decode msg of
-        (Just addresses :: Maybe [SockAddr]) -> mapM_ (write addrChan) addresses
-        Nothing -> pure ()
+        (Just addresses :: Maybe [SockAddr]) ->
+            const True <$> mapM_ (write addrChan) addresses
+        Nothing -> pure False
 
-decodeMsgAsKeyVal :: ByteString -> AddrChan -> StoreChan -> IO ()
+decodeMsgAsKeyVal :: ByteString -> AddrChan -> StoreChan -> IO Bool
 decodeMsgAsKeyVal msg addrChan store =
     case decode msg of
-        Just keyValPair -> put keyValPair addrChan store
-        Nothing -> pure ()
+        Just keyValPair -> const True <$> put keyValPair addrChan store
+        Nothing -> pure False
+
+decodeMsgAsTerminalInput :: ByteString -> AddrChan -> StoreChan -> IO ()
+decodeMsgAsTerminalInput msg addrChan store = do
+    let arguments = words $ show msg
+    case arguments of
+        "put":k:[v] ->
+            let pair = KeyValPair k v
+            in put pair addrChan store
+        "get":[k] -> get k store
+        _ -> putStrLn $ "Could not parse " ++ show msg
--- src/P2P/Get.hs
+++ src/P2P/Get.hs
@@ -5,7 +5,10 @@ import qualified Data.HashMap as HM
 import P2P.Chan
 import P2P.KeyValPair
 
-get :: Key -> StoreChan -> IO (Maybe Value)
+get :: Key -> StoreChan -> IO ()
 get k store = do
     pairs <- readEverything store
-    pure . HM.lookup k . HM.fromList $ toPair <$> pairs
+    case HM.lookup k . HM.fromList $ toPair <$> pairs of
+        Nothing ->
+            putStrLn $ unwords ["There is no value for the key", k, "yet."]
+        Just v -> putStrLn $ unwords ["The value for key", k, "is", v]
